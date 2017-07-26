3732f1e2bfec7d68af8cb1076a0e4e791fe46dac
Tomas Tomecek <ttomecek@redhat.com>
2017-07-26 07:03:08 +0200

    test fedora-release command
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- tests/test_fed_install.py
+++ tests/test_fed_install.py
@@ -1,5 +1,11 @@
 import subprocess
 
+"""
+This test suite is meant to be executed in a Fedora 26 environment
+
+go-srpm-macros is a good candidate for testing since it has zero dependencies
+"""
+
 
 def test_koji_build():
     build_nvr = 'units-2.14-1.fc27'
@@ -9,3 +15,16 @@ def test_koji_build():
     installed_package = subprocess.check_output(["rpm", "-q", "units"]).strip().decode()
 
     assert installed_package == build_nvra
+
+    subprocess.check_call(["dnf", "-y", "remove", "units"])
+
+
+def test_fedora_release():
+    package_name = 'hardlink'
+    cmd = ["fed-install", "-y", "fedora-release", "25", package_name]
+    subprocess.check_call(cmd)
+    installed_package = subprocess.check_output(["rpm", "-q", package_name]).strip().decode()
+
+    assert "fc25" in installed_package
+
+    subprocess.check_call(["dnf", "-y", "remove", package_name])
