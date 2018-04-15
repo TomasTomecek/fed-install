4e2c97a251f2353d418cf28d71fade78a9b9644f
Tomas Tomecek <ttomecek@redhat.com>
2018-04-15 10:50:15 +0200

    update and polish tests
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- tests/test_fed_install.py
+++ tests/test_fed_install.py
@@ -1,43 +1,41 @@
-import subprocess
-
 """
-This test suite is meant to be executed in a Fedora 26 environment
+This test suite is meant to be executed in a Fedora 27 environment
 
 go-srpm-macros is a good candidate for testing since it has zero dependencies
 """
 
+import subprocess
+
 
 def test_koji_build():
-    build_nvr = 'units-2.14-1.fc27'
+    build_nvr = 'units-2.14-5.fc27'
     build_nvra = build_nvr + '.x86_64'
     cmd = ["fed-install", "-y", "-v", "--enable-predefined-repos", "koji-build", build_nvr, "units"]
-    subprocess.check_call(cmd)
-    installed_package = subprocess.check_output(["rpm", "-q", "units"]).strip().decode()
-
-    assert installed_package == build_nvra
-
-    subprocess.check_call(["dnf", "-y", "remove", "units"])
+    try:
+        subprocess.check_call(cmd)
+        installed_package = subprocess.check_output(["rpm", "-q", "units"]).strip().decode()
+        assert installed_package == build_nvra
+    finally:
+        subprocess.call(["dnf", "-y", "remove", "units"])
 
 
 def test_fedora_release():
     package_name = 'hardlink'
-    cmd = ["fed-install", "-y", "-v", "from-release", "25", package_name]
-    subprocess.check_call(cmd)
-    installed_package = subprocess.check_output(["rpm", "-q", package_name]).strip().decode()
-
-    assert "fc25" in installed_package
-
-    subprocess.check_call(["dnf", "-y", "remove", package_name])
+    cmd = ["fed-install", "-y", "-v", "from-release", "26", package_name]
+    try:
+        subprocess.check_call(cmd)
+        installed_package = subprocess.check_output(["rpm", "-q", package_name]).strip().decode()
+        assert "fc26" in installed_package
+    finally:
+        subprocess.check_call(["dnf", "-y", "remove", package_name])
 
 
 def test_fedora_tag():
     package_name = 'hardlink'
-    cmd = ["fed-install", "-y", "-v", "koji-tag", "f25-build", package_name]
-    subprocess.check_call(cmd)
-    installed_package = subprocess.check_output(["rpm", "-q", package_name]).strip().decode()
-
-    assert "fc25" in installed_package
-
-    subprocess.check_call(["dnf", "-y", "remove", package_name])
-
-# TODO: add test which installs specific wine version
+    cmd = ["fed-install", "-y", "-v", "koji-tag", "f26-build", package_name]
+    try:
+        subprocess.check_call(cmd)
+        installed_package = subprocess.check_output(["rpm", "-q", package_name]).strip().decode()
+        assert "fc26" in installed_package
+    finally:
+        subprocess.check_call(["dnf", "-y", "remove", package_name])
